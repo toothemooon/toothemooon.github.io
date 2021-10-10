473e568bac5f8fdd64bb3ad2e57b388247ebf6c2
allen1357008 <abc510433622@gmail.com>
2021-10-10 11:39:29 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,44 +1,38 @@
-<script lang="ts">
-	import { text } from "svelte/internal";
-	
-		let firstName = 'Jimi';
-		let lastName = 'Hendr';
-		let beltColour = 'black';
-	
-		const handleClick = () => {
-			beltColour = 'orange';
-		}
-		
-		const handleInput = (e) => {
-			beltColour = e.target.value;
-		}
-	</script>
-	
-	<main>
-		<p>{beltColour} belt</p>
-		<input type="text" bind:value={firstName}>
-		<input type="text" bind:value={lastName}>
-		<input type='color' bind:value={beltColour}>
-	</main>
-	
-	<style>
-		main {
-			text-align: center;
-			padding: 1em;
-			max-width: 240px;
-			margin: 0 auto;
-		}
-	
-		h1 {
-			color: lightblue;
-			text-transform: uppercase;
-			font-size: 4em;
-			font-weight: 100;
-		}
-	
-		@media (min-width: 640px) {
-			main {
-				max-width: none;
-			}
-		}
-	</style>
+<script>
+	let people = [
+		{ name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
+		{ name: 'mario', beltColour: 'orange', age: 45, id: 2 },
+		{ name: 'luigi', beltColour: 'brown', age: 35, id: 3 },
+];
+
+const handleClick = (id) => {
+	people = people.filter((person) => person.id != id);
+};
+</script>
+
+<main>
+	{#each people as person (person.id)}
+		<div>
+			<h4>{person.name}</h4>
+			<p>{person.age} years old, {person.beltColour} belt.</p>
+			<button on:click={() => handleClick(person.id)}>delete</button>
+		</div>
+	{:else}
+		<p>There are no people to show...</p>
+	{/each}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+@media (min-width:640px) {
+	main {
+		max-width:none;
+	}
+}
+</style>
